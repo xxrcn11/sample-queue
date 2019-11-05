86ec667b984edad30abbf02fa5b7126470c95f89
xxrcn11 <xxrcn11@gmail.com>
2019-11-05 22:34:51 +0900

    modify in local

--- sample-queue/src/main/java/com/bt/sample/queue/service/PushPrint.java
+++ sample-queue/src/main/java/com/bt/sample/queue/service/PushPrint.java
@@ -3,5 +3,6 @@ package com.bt.sample.queue.service;
 public class PushPrint {
 	public static void ab() {
 		System.out.println("Add java class file push");
+		System.out.println("world with NM");
 	}
 }
