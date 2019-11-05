fe3e9692fb579985c6a1d84fca5db54d40ba332e
xxrcn11 <xxrcn11@gmail.com>
2019-11-05 21:38:54 +0900

    added print line

--- sample-queue/src/main/java/com/bt/sample/queue/SampleQueueApplication.java
+++ sample-queue/src/main/java/com/bt/sample/queue/SampleQueueApplication.java
@@ -2,13 +2,14 @@ package com.bt.sample.queue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class SampleQueueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleQueueApplication.class, args);
+		
+		System.out.println("Hi!");
 	}
 
 }
