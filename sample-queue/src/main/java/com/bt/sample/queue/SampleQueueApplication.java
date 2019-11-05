89db423274d723df2b4494fcc7539545021a6f68
xxrcn11 <xxrcn11@gmail.com>
2019-11-05 21:42:19 +0900

    hotfix branch change

--- sample-queue/src/main/java/com/bt/sample/queue/SampleQueueApplication.java
+++ sample-queue/src/main/java/com/bt/sample/queue/SampleQueueApplication.java
@@ -10,6 +10,7 @@ public class SampleQueueApplication {
 		SpringApplication.run(SampleQueueApplication.class, args);
 		
 		System.out.println("Hi!");
+		System.out.println("hotfix branch change!!");
 	}
 
 }
